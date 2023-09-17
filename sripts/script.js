25f12448818cb7257a0cfcd1f07f6298907c1007
VictorFRIBOULET <victor.friboulet@etu.unicaen.fr>
2023-09-17 20:20:10 +0200

    correction problèmes et amelioration affichage

--- sripts/script.js
+++ sripts/script.js
@@ -1,35 +1,43 @@
 const horloge = document.getElementById("timer");
-var state = "travail";
+let work = true;
 var r = document.querySelector(':root');
 let duree_travail=5;
 let duree_pause=5;
 let timer = duree_travail;
 let timerproperty ;
+let playButton = document.getElementById("play");
+let stopButton = document.getElementById("stop");
 horloge.innerHTML = new Date(1000 * timer).toISOString().substring(14, 19);
-document.getElementById("play").addEventListener("click", function(){
+playButton.addEventListener("click", function(){
     clearInterval(timerproperty);
     timerproperty=setInterval(passe_temps, 1000);
+    playButton.style.setProperty("display","none");
+    stopButton.style.setProperty("display","block");
+
 });
-document.getElementById("stop").addEventListener("click", function(){
+stopButton.addEventListener("click", function(){
     clearInterval(timerproperty);
-    if (state="travail"){
-        timer = duree_pause;
+    if (work){
+        timer = duree_travail;
     }else{
-        state="travail";
+        work = true;
         timer = duree_travail;
     }
     horloge.innerHTML = new Date(1000 * timer).toISOString().substring(14, 19);
+    stopButton.style.setProperty("display","none");
+    playButton.style.setProperty("display","block");
 });
 
 function passe_temps(){
     timer--;
-    if (timer<=0){
-        if (state="travail"){
-            state="pause";
+    if (timer<0){
+        if (work){
+            work = false;
             timer = duree_pause;
             change_color();
+            
         }else{
-            state="travail";
+            work = true;
             timer = duree_travail;
             change_color();
         }
@@ -39,15 +47,17 @@ function passe_temps(){
 }
 
 function change_color(){
-    if (state="travail"){
-        document.documentElement.style.setProperty('--main-bg-color','#e13d3d');
-        document.documentElement.style.setProperty('--affichage-bg-color','#aa0000');
-        document.documentElement.style.setProperty('--button-hover-color','#e13d3d');
-        document.documentElement.style.setProperty('--button-color','#e13d3d');
+    if (work){
+        horloge.style.setProperty("color","red");
+        document.body.style.setProperty('--main-bg-color','#e13d3d');
+        document.body.style.setProperty('--affichage-bg-color','#aa0000');
+        document.body.style.setProperty('--button-hover-color','#ff9999');
+        document.body.style.setProperty('--button-color','#aa0000');
     }else{
-        document.documentElement.style.setProperty('--main-bg-color','#94c7a8');
-        document.documentElement.style.setProperty('--affichage-bg-color','#69967b');
-        document.documentElement.style.setProperty('--button-hover-color','#c0f0d3');
-        document.documentElement.style.setProperty('--button-color','#e13d3d');
+        horloge.style.setProperty("color","seagreen");
+        document.body.style.setProperty('--main-bg-color','#94c7a8');
+        document.body.style.setProperty('--affichage-bg-color','#69967b');
+        document.body.style.setProperty('--button-hover-color','#c0f0d3');
+        document.body.style.setProperty('--button-color','#69967b');
     }
 }
