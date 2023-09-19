d3c1b6aa42c2de2e80df3a2547764745922205de
VictorFRIBOULET <victor.friboulet@etu.unicaen.fr>
2023-09-19 08:33:35 +0200

    parametres et slider+ passage à un affichage h:m:s

--- sripts/script.js
+++ sripts/script.js
@@ -1,13 +1,31 @@
+let duree_travail = 1800;
+let duree_pause = 1800;
+try {
+    const stockage = window.localStorage;
+
+    if (stockage.getItem("duree_travail") !== null) {
+        duree_travail = parseInt(stockage.getItem("duree_travail"));
+    }
+
+    if (stockage.getItem("duree_pause") !== null) {
+        duree_pause = parseInt(stockage.getItem("duree_pause"));
+    }
+} catch (error) {
+    console.log(error);
+}
+
+
+
+
 const horloge = document.getElementById("timer");
 let work = true;
 var r = document.querySelector(':root');
-let duree_travail=5;
-let duree_pause=5;
+
 let timer = duree_travail;
 let timerproperty ;
 let playButton = document.getElementById("play");
 let stopButton = document.getElementById("stop");
-horloge.innerHTML = new Date(1000 * timer).toISOString().substring(14, 19);
+horloge.innerHTML = new Date(1000 * timer).toISOString().substring(11, 19);
 playButton.addEventListener("click", function(){
     clearInterval(timerproperty);
     timerproperty=setInterval(passe_temps, 1000);
@@ -23,7 +41,7 @@ stopButton.addEventListener("click", function(){
         work = true;
         timer = duree_travail;
     }
-    horloge.innerHTML = new Date(1000 * timer).toISOString().substring(14, 19);
+    horloge.innerHTML = new Date(1000 * timer).toISOString().substring(11, 19);
     stopButton.style.setProperty("display","none");
     playButton.style.setProperty("display","block");
 });
@@ -42,7 +60,7 @@ function passe_temps(){
             change_color();
         }
     }
-    horloge.innerHTML = new Date(1000 * timer).toISOString().substring(14, 19);
+    horloge.innerHTML = new Date(1000 * timer).toISOString().substring(11, 19);
 
 }
 
@@ -60,4 +78,42 @@ function change_color(){
         document.body.style.setProperty('--button-hover-color','#c0f0d3');
         document.body.style.setProperty('--button-color','#69967b');
     }
+}
+//boutons paramètres
+let parametres = document.getElementById("parametres");
+let parametres_button = document.getElementById("parametres_button");
+parametres.style.display = "none";
+parametres_button.addEventListener("click", function(){
+    if (parametres.style.display == "none") {
+        parametres.style.display = "block";
+    } else {
+        parametres.style.display = "none";
+    }
+});
+
+
+
+
+// Slider paramètres
+
+let slider_travail = document.getElementById("SliderTempsTravail");
+let slider_pause = document.getElementById("SliderTempsPause");
+let output_travail = document.getElementById("tempsTravail");
+let output_pause = document.getElementById("tempsPause");
+slider_travail.value = duree_travail/60;
+slider_pause.value = duree_pause/60;
+output_travail.innerHTML = slider_travail.value;
+output_pause.innerHTML = slider_pause.value;
+slider_travail.oninput = function() {
+    work = true;
+    output_travail.innerHTML = this.value;
+    duree_travail = this.value*60;
+    stockage.setItem("duree_travail", duree_travail);
+    timer = duree_travail;
+    horloge.innerHTML = new Date(1000 * timer).toISOString().substring(11, 19);
+}
+slider_pause.oninput = function() {
+    output_pause.innerHTML = this.value;
+    duree_pause = this.value;
+    stockage.setItem("duree_pause", duree_pause);
 }
